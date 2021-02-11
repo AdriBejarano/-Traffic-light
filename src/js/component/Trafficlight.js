aca478ac2a991834508fa31f8710fd4f86168c33
AdriBejarano <adriana_bt@hotmail.com>
2021-02-11 23:29:21 +0000

    logrado

---
+++ src/js/component/Trafficlight.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const Trafficlight = () => {
+	//    variable de estado - modificador
+	const [color, setColor] = useState("red");
+	return (
+		<div id="traffictop">
+			<div className="stickArea">
+				<div className="vl"></div>
+			</div>
+			<div className="container">
+				<div
+					className={"red light " + (color == "red" ? "active" : "")}
+					onClick={() => {
+						setColor("red");
+					}}
+				/>
+				<div
+					className={
+						"yellow light " + (color == "yellow" ? "active" : "")
+					}
+					onClick={() => {
+						setColor("yellow");
+					}}
+				/>
+				<div
+					className={
+						"green light " + (color == "green" ? "active" : "")
+					}
+					onClick={() => {
+						setColor("green");
+					}}
+				/>
+			</div>
+		</div>
+	);
+};
